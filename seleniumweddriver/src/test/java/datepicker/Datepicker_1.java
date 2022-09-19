d1974adcd3ec7f9ae703ce1ab5e8a094af761473
manohar <manoharch99@gmail.com>
2022-09-19 15:02:33 +0530

    date changed

--- seleniumweddriver/src/test/java/datepicker/Datepicker_1.java
+++ seleniumweddriver/src/test/java/datepicker/Datepicker_1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 //type 1 clalender 
+//additional calender comment for testing bbbmghjg
 public class Datepicker_1 {
 
 	public static void main(String[] args) throws InterruptedException {
