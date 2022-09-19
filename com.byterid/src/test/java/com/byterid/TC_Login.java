eb03431eb811e6bee297858a44c502c556632ca0
manohar <manoharch99@gmail.com>
2022-09-19 14:42:17 +0530

    changed

--- com.byterid/src/test/java/com/byterid/TC_Login.java
+++ com.byterid/src/test/java/com/byterid/TC_Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
+//manuimport io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class TC_Login {
